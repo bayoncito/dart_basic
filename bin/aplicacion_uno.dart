80730a377bbbcf9474d2c6376520508e8a83ccf2
Andres Bayon <johana.bayonr@utadeo.edu.co>
2024-02-08 19:28:18 -0500

    Primer Commit

---
+++ bin/aplicacion_uno.dart
@@ -0,0 +1,5 @@
+import 'package:aplicacion_uno/aplicacion_uno.dart' as aplicacion_uno;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aplicacion_uno.calculate()}!');
+}
---
+++ test/aplicacion_uno_test.dart
@@ -0,0 +1,8 @@
+import 'package:aplicacion_uno/aplicacion_uno.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
