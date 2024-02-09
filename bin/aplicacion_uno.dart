d24691568d13e2d830b3d2a428d7141fc9505aa2
Andres Bayon <johana.bayonr@utadeo.edu.co>
2024-02-08 19:38:11 -0500

    Actualizacion

--- bin/aplicacion_uno.dart
+++ bin/aplicacion_uno.dart
@@ -1,5 +1,44 @@
-import 'package:aplicacion_uno/aplicacion_uno.dart' as aplicacion_uno;
-
 void main(List<String> arguments) {
-  print('Hello world: ${aplicacion_uno.calculate()}!');
+  String variableTexto = "Hola mundo";
+  int numeroEnteros = 15;
+  double numeroDouble = 3.4;
+  bool esVerdad = true;
+  
+  var esUnTexto = "JAJAJAJA xde";
+  dynamic esUnNumero = 14;
+
+  esUnNumero = "Texto prras";
+
+  print(variableTexto);
+
+
+  String? name;
+
+  var arreglo = [1,2,3,4,5,6];
+
+  List<int> lista = [1,2,3,4,5,6];
+
+  List<String> caracteres = ["","","","",""];
+
+  //comentarios aca :33 
+
+  /*
+  el for y el while es igual que en java
+   */
+
+
+  if(numeroEnteros == 15){
+    print("thats real bro");
+  }else if(numeroEnteros == 1){
+    print("es fake bro");
+  }else{
+    print("eres puto y no sabes :v");
+  }
+
+  switch(numeroEnteros){
+    case 15:
+    print("Thats real bro");
+    break;
+  }
+
 }
